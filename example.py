46e4e42bd34aeaecd03e8e024d719e4fb4c070c2
jtanningbed <jtan92@gmail.com>
2024-11-27 23:19:12 -0500

    example.py

--- example.py
+++ example.py
@@ -13,15 +13,23 @@ from neo4j_spacy_procedures import SpacyNLPProcedure
 #     print(f"- {entity['text']} ({entity['type']})")
 
 
-
 # Connect to your running container
 proc = SpacyNLPProcedure("bolt://localhost:7687", "neo4j", "password")
 
 # Register the procedure
 proc.register_procedures()
 
-# # Test with a simple text
-# text = "Apple is looking at buying U.K. startup for $1 billion"
-# with proc.driver.session() as session:
-#     result = session.run("CALL custom.nlp.spacy.entities($text)", text=text)
-#     print(result.single())
+# Test with a simple text
+text = "Apple is looking at buying U.K. startup for $1 billion"
+with proc.driver.session() as session:
+    result = session.run("CALL custom.nlp.spacy.entities($text)", text=text)
+    print(result.single())
+
+
+# docker run \
+#     --name neo4j-spacy \
+#     -p 7474:7474 -p 7687:7687 \
+#     -e NEO4J_AUTH=neo4j/password \
+#     -e "NEO4J_dbms_security_procedures_unrestricted=*" \
+#     -e NEO4J_PLUGINS=\[\"apoc-extended\"\] \
+#     neo4j:latest
